6ee774ff44744b67aaf87583e5f1923491bf6751
gabriel.sales <gabriel.sales@basis.com.br>
2018-09-03 19:06:03 -0300

    add conexao com banco

---
+++ src/br/com/terranostra/jdbc/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package br.com.terranostra.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+  public Connection getConnection() {
+      try {
+          return DriverManager.getConnection(
+                  "jdbc:postgresql://localhost:5432/mpog", "geoidentificacao", "geoidentificacao");
+      } catch (SQLException e) {
+          throw new RuntimeException(e);
+      }
+  }
+}
